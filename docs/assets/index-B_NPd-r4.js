cca6ac8b029239a9f773c2fb4e7b58e48eb2e7f3
mtt999 <motlagh@illinois.edu>
2026-06-03 22:57:23 -0500

    Add delete account to staff/student profiles, fix T&C re-prompt on refresh

---
+++ docs/assets/index-B_NPd-r4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bu3Kup7y.js","assets/index-C_UGFb25.js","assets/index-W5updjjt.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-C_UGFb25.js";const _=e("Browser",{web:()=>o(()=>import("./web-Bu3Kup7y.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
