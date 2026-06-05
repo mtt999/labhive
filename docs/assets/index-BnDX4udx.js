4183bce391c66ebb3c680e1d5eadaf8419d4185d
mtt999 <motlagh@illinois.edu>
2026-06-05 12:11:32 -0500

    Rename dashboard icons across all screens

---
+++ docs/assets/index-BnDX4udx.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CowUuCez.js","assets/index-Cc-1HgEY.js","assets/index-W5updjjt.css"])))=>i.map(i=>d[i]);
+import{r,_ as o}from"./index-Cc-1HgEY.js";const _=r("JailbreakRoot",{web:()=>o(()=>import("./web-CowUuCez.js"),__vite__mapDeps([0,1,2])).then(e=>new e.JailbreakRootWeb),electron:()=>o(()=>import("./web-CowUuCez.js"),__vite__mapDeps([0,1,2])).then(e=>new e.JailbreakRootWeb)});export{_ as JailbreakRoot};
---
+++ docs/assets/web-S3dQZ_I5.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-Cc-1HgEY.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
